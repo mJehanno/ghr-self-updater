ed26c8fd6686ac9d9c13e2fc9bfd054ac40b52bc
mjehanno <mathob.jehanno@hotmail.fr>
2024-02-25 09:50:25 +0100

    feat(updater): add a checkLatest method that will check if current version is the latest
    it also improve godoc comments

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package selfupdater
 import (
 	"context"
 	"net/http"
+	"strings"
 
 	"github.com/blang/semver"
 	"github.com/google/go-github/v59/github"
@@ -18,27 +19,30 @@ type Updater struct {
 	gclient *github.Client
 }
 
-type updaterOpts func(*Updater)
+// UpdaterOpts represent an option you can pass to [Updater] constructor.
+// It uses functional option pattern so the underlying type is `func(*Updater)`.
+type UpdaterOpts func(*Updater)
 
-// WithContext will pass the given context to an Updater instance.
-func WithContext(ctx context.Context) updaterOpts {
+// WithContext will pass the given context to an [Updater] instance.
+func WithContext(ctx context.Context) UpdaterOpts {
 	return func(u *Updater) {
 		u.ctx = ctx
 	}
 }
 
-// WithHttpClient will pass the given *http.Client to an Updater instance.
-func WithHttpClient(client *http.Client) updaterOpts {
+// WithHttpClient will pass the given *http.Client to an [Updater] instance.
+func WithHttpClient(client *http.Client) UpdaterOpts {
 	return func(u *Updater) {
 		u.gclient = github.NewClient(client)
 	}
 }
 
 // New creates a new instance of Updater.
-// It needs the owner and repo name to work and the current version of your app (in semver format ->https://github.com/blang/semver )
+// It needs the owner and repo name to work and the current version of your app (in semver format ->  [semver package])
 // You can pass some options (WithContext, WithHttpClient) so that the updater can fits your need.
 // If you don't, the Updater will use context.Background and http.DefaultClient by default.
-func New(owner, repo string, current semver.Version, options ...updaterOpts) *Updater {
+// [semver package]: https://github.com/blang/semver
+func New(owner, repo string, current semver.Version, options ...UpdaterOpts) *Updater {
 	u := &Updater{
 		Owner:   owner,
 		Repo:    repo,
@@ -52,4 +56,21 @@ func New(owner, repo string, current semver.Version, options ...updaterOpts) *Up
 	}
 
 	return u
+}
+
+// CheckLatest will check if the current version is the latest.
+// It returns a boolean, the corresponding github release (if there is one) and an error.
+// To avoid wrong behaviour, it returns true if an error is encountered.
+func (u *Updater) CheckLatest() (bool, *github.RepositoryRelease, error) {
+	rel, _, err := u.gclient.Repositories.GetLatestRelease(u.ctx, u.Owner, u.Repo)
+	if err != nil {
+		return true, nil, err
+	}
+
+	latest, err := semver.Parse(strings.ReplaceAll(rel.GetTagName(), "v", ""))
+	if err != nil {
+		return true, nil, err
+	}
+
+	return latest.EQ(u.Current), rel, nil
 }
