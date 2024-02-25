f2ce387335c7f9cf1d89d6c85e12ade54e77a024
mjehanno <mathob.jehanno@hotmail.fr>
2024-02-25 09:32:51 +0100

    feat(updater): add updater definition and constructor

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/mJehanno/ghr-self-updater
 
 go 1.21.1
+
+require (
+	github.com/blang/semver v3.5.1+incompatible
+	github.com/google/go-github/v59 v59.0.1-0.20240217151021-73422173c633
+)
+
+require github.com/google/go-querystring v1.1.0 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,55 @@
+// Package selfupdater implements logic behind self-updating App. It uses github releases to update your app.
 package selfupdater
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/blang/semver"
+	"github.com/google/go-github/v59/github"
+)
+
+// Updater is the main structure in charge to check latest version and update your app.
+type Updater struct {
+	Owner   string
+	Repo    string
+	Current semver.Version
+	ctx     context.Context
+	gclient *github.Client
+}
+
+type updaterOpts func(*Updater)
+
+// WithContext will pass the given context to an Updater instance.
+func WithContext(ctx context.Context) updaterOpts {
+	return func(u *Updater) {
+		u.ctx = ctx
+	}
+}
+
+// WithHttpClient will pass the given *http.Client to an Updater instance.
+func WithHttpClient(client *http.Client) updaterOpts {
+	return func(u *Updater) {
+		u.gclient = github.NewClient(client)
+	}
+}
+
+// New creates a new instance of Updater.
+// It needs the owner and repo name to work and the current version of your app (in semver format ->https://github.com/blang/semver )
+// You can pass some options (WithContext, WithHttpClient) so that the updater can fits your need.
+// If you don't, the Updater will use context.Background and http.DefaultClient by default.
+func New(owner, repo string, current semver.Version, options ...updaterOpts) *Updater {
+	u := &Updater{
+		Owner:   owner,
+		Repo:    repo,
+		Current: current,
+		ctx:     context.Background(),
+		gclient: github.NewClient(http.DefaultClient),
+	}
+
+	for _, optn := range options {
+		optn(u)
+	}
+
+	return u
+}
